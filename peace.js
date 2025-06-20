f487b2ccedb9cb2d39b0b4f930732ed66b119b77
xl-15 <lewv32@gmail.com>
2025-06-19 22:47:04 -0700

    got all my variables ready after the dom loads

--- peace.js
+++ peace.js
@@ -5,32 +5,30 @@
 // I will try to make this work with the HTML file provided
 // I will also try to make it work with the CSS file provided
 // I will also try to make it work with the PHP file provided
-//hahha theres no phph file im just rrly bad at this
+// hahha theres no php file im just rrly bad at this
 
 
-//this will start by waiting for the DOM to be fully loaded
-// and then it will add event listeners to the radio buttons and cards
-// and will toggle the selected class on the clicked card and radio button
-// and will also toggle the selected class on the other cards and radio buttons
-// and will also update the hidden input field with the selected card's value
-// and will also update the hidden input field with the selected card's value
-let tester123 = 'test123';
-document.addEventListener('DOMContentLoaded',() => function() {
-    //this gets the class from EVERY label
-    const allCards = document.querySelectorAll('.card');
-    //this gets the class from EVERY input/radio button
-    const allRadioButtons = document.querySelectorAll("input[name='bookingCard']");
-    //the one will get the submit button
-    const submitButton = document.querySelector('submitBookingButton');
+let tester123 = 'yes,test123';
+// This is a comment to just add a useless variable to the file
 
-    //this var will temporarily hold the selected card
-    let currentSelectedId = null;
 
-    //This is a special block of code (a 'function') that we can call whenever
-    //we need to update how the cards look based on which radio button is checked.
-    const updateCardSelectionUi = () => {
-        allCards.forEach(card =>{
-            
-        })
-    }
+document.addEventListener('DOMContentLoaded',() => {
+
+    console.log('Document is ready!');
+
+
+    const allProductCards = document.querySelectorAll('productCard');
+    console.log("Found these product cards:", allProductCards);
+
+    const allRadioInputs = document.querySelectorAll('input[name = "bookingCard"]');
+    console.log("Found these radio inputs:", allRadioInputs);
+
+    const checkoutButton = document.querySelector('submitBookingButton');
+    console.log("Found the checkout button:", checkoutButton);
+
+    const productForm = document.getElementById('productSelectionForm');
+    console.log("Found the product selection form:", productForm);
+
+    let curentlySelectedProduct = null;
+    console.log("found the selected product",curentlySelectedProduct);
 });
